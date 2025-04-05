6cf85d1c751a65c782d0587494d52f40cfdcd2ad
Cecilia <cecilia.delacamara@gmail.com>
2025-04-05 18:55:29 +0200

    Resuleto ejercicio 1 del reto

---
+++ Retos.py
@@ -0,0 +1,18 @@
+# 📌 1. RETO 30 DÍAS ABRIL: DIA 1 | Redondeo
+# ------------------------------------------
+# En Grado Superior las notas se tienen que poner como un número entero, así que un día podrás ser el profe de entornos
+# y podrás disfrutar del placer de poner un 7 a un estudiante con un 7,49 en el examen.
+# Desarrolla un algoritmo que, dado un número decimal, devuelve su resultado entero redondeado siguiendo estas normas:
+# Todos los números decimales por debajo de ,5 se redondean a la baja.
+# Los que tengan decimales desde ,5 a superior, se redondean al alza.
+#
+# Ejemplo:
+# Si el usuario introduce 4,49, el programa debe devolver un 4
+# Si el usuario introduce 9,5 el programa debe devolver un 10
+
+import math
+nota = float(input("¿Qué nota has sacado?")) # Convierte lo que escribe el usuario en decimales
+if (nota - int(nota) >= 0.5): #int(nota) quita los decimales y nota - int(nota) da solo la parte decimal
+    print(int(nota) + 1)
+else:
+    print(int(nota))
